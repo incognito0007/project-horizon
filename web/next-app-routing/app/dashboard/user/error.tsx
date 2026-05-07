a6441c00194c5b4c6d32961b729e420558280b45
incognito0007 <anandaankit480@gmail.com>
2026-05-07 13:09:22 +0530

    [web] feat: add ErrorPage component and adjust loading simulation in UserPage

---
+++ web/next-app-routing/app/dashboard/user/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+export default function ErrorPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Error</h1>
+      <p>Sorry, an error occurred while loading the dashboard User page.</p>
+    </div>
+  );
+}
