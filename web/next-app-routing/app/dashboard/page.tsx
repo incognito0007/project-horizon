c159390b6e5116481bba77499829590f81f36291
incognito0007 <anandaankit480@gmail.com>
2026-05-07 12:34:30 +0530

    [web] feat: update DashboardPage to include client-side rendering directive

--- web/next-app-routing/app/dashboard/page.tsx
+++ web/next-app-routing/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+"use client"; // This is a client component, which means it will be rendered on the client side, and will be sent to the client. So you can write code here that you want to be exposed to the client, such as event handlers, state management, etc. example: any console.log here will be visible in the browser console.
+
 const DashboardPage = () => {
   // Whatever written here will be rendered on server side, and will not be sent to the client. So you can write code here that you don't want to be exposed to the client, such as database queries, API calls, etc. example: any console.log here will not be visible in the browser console, but will be visible in the server console.
 
