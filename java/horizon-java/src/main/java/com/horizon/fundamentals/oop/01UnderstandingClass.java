1abb86a9b03889f8304b70cdc4679ad0c016dbe5
incognito0007 <anandaankit480@gmail.com>
2026-05-08 17:56:59 +0530

    [java] feat: add 01UnderstandingClass to demonstrate basic array usage in Java and to understand why we need class

---
+++ java/horizon-java/src/main/java/com/horizon/fundamentals/oop/01UnderstandingClass.java
@@ -0,0 +1,15 @@
+package com.horizon.fundamentals.oop;
+
+public class 01UnderstandindClass{
+    public static void main(String[] args) {
+        // Store 5 roll nos
+        int[] rollNos = new int[5];
+
+        // Store 5 names
+        String[] names = new String[5];
+
+        // data of 5 students: {roll no, name, marks}
+        int[] marks = new int[5];
+        
+    }
+}
