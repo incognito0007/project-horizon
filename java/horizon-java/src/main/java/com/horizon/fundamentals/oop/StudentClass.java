32c4c13eb30c72173adab48c886c8940fded6f29
incognito0007 <anandaankit480@gmail.com>
2026-05-08 18:48:25 +0530

    [java] feat: update StudentClass to clarify marks initialization and enhance student3 demonstration

--- java/horizon-java/src/main/java/com/horizon/fundamentals/oop/StudentClass.java
+++ java/horizon-java/src/main/java/com/horizon/fundamentals/oop/StudentClass.java
@@ -12,7 +12,7 @@ public class StudentClass {
         System.out.println(student1); // This will print the reference of the student1 object because it is not initialized. It will print something like com.horizon.fundamentals.oop.Students@15db9742
         System.out.println(student1.rollNo); // This will print 0 because rollNo is an int and it is initialized to 0 by default.
         System.out.println(student1.name); // This will print null because name is a String and it is initialized to null by default.
-        System.out.println(student1.marks); // This will print 0 because marks is an int and it is initialized to 0 by default.
+        System.out.println(student1.marks); // This will print 0 because marks is an int and it is initialized to 0 by default.  --> This will print 70 because marks is now initialized to 70.
         
         Students student2 = new Students();
         student2.rollNo = 2;
@@ -22,6 +22,13 @@ public class StudentClass {
         System.out.println(student2.name); // This will print Alice
         System.out.println(student2.marks); // This will print 90
 
+        Students student3 = new Students();
+        student3.rollNo = 3;
+        student3.name = "Bob";
+        System.out.println(student3.rollNo); // This will print 3
+        System.out.println(student3.name); // This will print Bob
+        System.out.println(student3.marks); // This will print 70 because marks is initialized to 70.
+
         // System.out.println(Arrays.toString(students)); // This will print null because student1 is not initialized.
     }
 }
@@ -29,5 +36,5 @@ public class StudentClass {
 class Students {
     int rollNo;
     String name;
-    int marks;
+    int marks = 70; // We can also initialize the marks variable to a default value of 70.
 }
