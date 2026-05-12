859ce80696b4f454d8fac179a705c031b4fec2ae
incognito0007 <anandaankit480@gmail.com>
2026-05-12 17:07:43 +0530

    [dotnet] refactor: implementing constructor  dependency injection for Person class to reduce coupling

--- dot-net/horizon-dotnet/src/HorizonDotnet.Console/Fundamentals/DependencyInjection/DemoApp.cs
+++ dot-net/horizon-dotnet/src/HorizonDotnet.Console/Fundamentals/DependencyInjection/DemoApp.cs
@@ -2,7 +2,10 @@ public class DemoApp
 {
     public static void Run(string[] args)
     {
-        Person person = new Person();
+        // creating an instance of the Home class
+        Home home = new Home();
+        // creating an instance of the Person class and injecting the Home class into the constructor of the Person class
+        Person person = new Person(home);
         person.TakeRefugee();
         person.Study();
         person.GetTreatment();
--- dot-net/horizon-dotnet/src/HorizonDotnet.Console/Fundamentals/DependencyInjection/Person.cs
+++ dot-net/horizon-dotnet/src/HorizonDotnet.Console/Fundamentals/DependencyInjection/Person.cs
@@ -4,11 +4,13 @@ public class Person
     private School _school;
     private Hospital _hospital;
 
-    public Person ()
+    public Person (Home home)
     {
         // highly coupling the Person class with Home, School and Hospital classes
         //any change in the Home, School or Hospital class will require a change in the Person class as well which increases the maintenance cost and reduces the flexibility of the code
-        _home = new Home();
+        //Instead of this, we can do dependency injection to inject the Home, School and Hospital classes into the Person class which will reduce the coupling and increase the flexibility of the code
+        // _home = new Home(); // this is called tight coupling because the Person class is tightly coupled with the Home class
+        _home = home; // this is called loose coupling because the Person class is loosely coupled with the Home class and this is constructor injection because we are injecting the Home class through the constructor of the Person class
         _school = new School();
         _hospital = new Hospital();
     }
