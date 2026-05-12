9ef1979bc81bd84bfe9a59e9477f8db13dbb0d3d
incognito0007 <anandaankit480@gmail.com>
2026-05-12 17:06:03 +0530

    [dotnet] feat: implement DemoApp class with methods for person actions

---
+++ dot-net/horizon-dotnet/src/HorizonDotnet.Console/Fundamentals/DependencyInjection/DemoApp.cs
@@ -0,0 +1,10 @@
+public class DemoApp
+{
+    public static void Run(string[] args)
+    {
+        Person person = new Person();
+        person.TakeRefugee();
+        person.Study();
+        person.GetTreatment();
+    }
+}
